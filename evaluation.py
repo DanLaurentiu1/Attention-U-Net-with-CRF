424cb6b5c33ce46c0758f3dbd5c7696065229a46
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2024-11-24 19:04:35 +0200

    add new evaluation metric

--- evaluation.py
+++ evaluation.py
@@ -7,20 +7,20 @@ from torchvision import transforms
 SMOOTH = 1e-6
 
 
-def iou(outputs: torch.Tensor, labels: torch.Tensor):
+def iou(outputs, labels):
     outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
 
-    intersection = (outputs & labels).float().sum((1, 2))
-    union = (outputs | labels).float().sum((1, 2))
+    intersection = (outputs.bool() & labels.bool()).float().sum((1, 2))
+    union = (outputs.bool() | labels.bool()).float().sum((1, 2))
 
     iou = (intersection + SMOOTH) / (union + SMOOTH)
 
-    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
+    # thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
 
-    return thresholded
+    return iou
 
 
-def single_image_inference(image_pth, mask_path, model_pth, device):
+def single_image_evaluation(image_pth, mask_path, model_pth, device):
     model = UNet(in_channels=3, out_channels=1).to(device)
     model.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
 
@@ -34,17 +34,15 @@ def single_image_inference(image_pth, mask_path, model_pth, device):
     img = img.unsqueeze(0)
 
     pred_mask = model(img)
-
+    pred_mask = torch.where(pred_mask <= 1, torch.tensor(1.0), torch.tensor(0.0))
+    mask = torch.where(mask < 0.5, torch.tensor(1.0), torch.tensor(0.0))
     print(iou(pred_mask, mask))
 
-    # pred_mask = torch.where((pred_mask > -2.4) & (pred_mask < -1.3), torch.tensor(0.0), torch.tensor(1.0))
-
-
 if __name__ == "__main__":
-    SINGLE_IMG_PATH = "C:\\Users\\Lau\\PycharmProjects\\Attention-U-Net-with-CRF\\beachlitter_dataset_2022_smaller\\beachlitter\\images\\000108.jpg"
-    SINGLE_MASK_PATH = "C:\\Users\\Lau\\PycharmProjects\\Attention-U-Net-with-CRF\\beachlitter_dataset_2022_smaller\\beachlitter\\maskpngs\\000108.png"
+    SINGLE_IMG_PATH = "C:\\Users\\Lau\\PycharmProjects\\Attention-U-Net-with-CRF\\beachlitter_dataset_2022_smaller_binary\\beachlitter\\images\\000104.jpg"
+    SINGLE_MASK_PATH = "C:\\Users\\Lau\\PycharmProjects\\Attention-U-Net-with-CRF\\beachlitter_dataset_2022_smaller_binary\\beachlitter\\maskpngs\\000104.png"
     DATA_PATH = "./data"
     MODEL_PATH = "C:\\Users\\Lau\\PycharmProjects\\Attention-U-Net-with-CRF\\model\\model.pth"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    single_image_inference(SINGLE_IMG_PATH, SINGLE_MASK_PATH, MODEL_PATH, device)
+    single_image_evaluation(SINGLE_IMG_PATH, SINGLE_MASK_PATH, MODEL_PATH, device)
